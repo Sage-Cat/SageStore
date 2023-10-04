56ea16c16f79ca722c5c7d495a8e09f77cdd5f35
Volodymyr <43709724+Sage-Cat@users.noreply.github.com>
2023-10-04 14:47:51 +0300

    Updated puml and database sql

--- scripts/create_db.sql
+++ scripts/create_db.sql
@@ -1,112 +1,123 @@
--- Enable Foreign Key Support
-PRAGMA foreign_keys = ON;
-
--- Create Tables with Constraints
--- Roles Table
-CREATE TABLE IF NOT EXISTS Roles (
-    RoleID INTEGER PRIMARY KEY AUTOINCREMENT,
-    Name TEXT NOT NULL UNIQUE
+-- Create Roles table
+CREATE TABLE Roles (
+  RoleID INT PRIMARY KEY AUTO_INCREMENT,
+  Name VARCHAR(50) NOT NULL
 );
 
--- Users Table with Foreign Key Constraints and Unique username
-CREATE TABLE IF NOT EXISTS Users (
-    UserID INTEGER PRIMARY KEY AUTOINCREMENT,
-    Username TEXT UNIQUE NOT NULL,
-    Password TEXT NOT NULL,  -- Note: Should be stored as a hashed value
-    RoleID INTEGER NOT NULL,
-    FOREIGN KEY (RoleID) REFERENCES Roles (RoleID) ON DELETE RESTRICT ON UPDATE CASCADE
+-- Create Users table
+CREATE TABLE Users (
+  UserID INT PRIMARY KEY AUTO_INCREMENT,
+  Username VARCHAR(50) UNIQUE NOT NULL,
+  Password VARCHAR(255) NOT NULL, -- Assuming encrypted password
+  RoleID INT,
+  FOREIGN KEY (RoleID) REFERENCES Roles(RoleID)
 );
 
-CREATE UNIQUE INDEX idx_users_username ON Users(Username);
-
--- User Logs
-CREATE TABLE IF NOT EXISTS UserLogs (
-    LogID INTEGER PRIMARY KEY AUTOINCREMENT,
-    UserID INTEGER NOT NULL,
-    Action TEXT NOT NULL,
-    Timestamp DATETIME DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (UserID) REFERENCES Users (UserID) ON DELETE CASCADE ON UPDATE CASCADE
+-- Create UserLogs table
+CREATE TABLE UserLogs (
+  LogID INT PRIMARY KEY AUTO_INCREMENT,
+  UserID INT,
+  Action VARCHAR(100),
+  Timestamp DATETIME,
+  FOREIGN KEY (UserID) REFERENCES Users(UserID)
 );
 
--- Contacts
-CREATE TABLE IF NOT EXISTS Contacts (
-    ContactID INTEGER PRIMARY KEY AUTOINCREMENT,
-    Name TEXT NOT NULL,
-    Type TEXT NOT NULL,
-    Email TEXT UNIQUE,
-    Phone TEXT UNIQUE
+-- Create Contacts table
+CREATE TABLE Contacts (
+  ContactID INT PRIMARY KEY AUTO_INCREMENT,
+  Name VARCHAR(100),
+  Type VARCHAR(50),
+  Email VARCHAR(100),
+  Phone VARCHAR(20)
 );
 
--- Contact Cars
-CREATE TABLE IF NOT EXISTS ContactCars (
-    CarID INTEGER PRIMARY KEY AUTOINCREMENT,
-    ContactID INTEGER NOT NULL,
-    LicensePlate TEXT UNIQUE NOT NULL,
-    Model TEXT NOT NULL,
-    FOREIGN KEY (ContactID) REFERENCES Contacts (ContactID) ON DELETE CASCADE ON UPDATE CASCADE
+-- Create ContactCars table
+CREATE TABLE ContactCars (
+  CarID INT PRIMARY KEY AUTO_INCREMENT,
+  ContactID INT,
+  LicensePlate VARCHAR(20),
+  Model VARCHAR(50),
+  FOREIGN KEY (ContactID) REFERENCES Contacts(ContactID)
 );
 
--- Suppliers
-CREATE TABLE IF NOT EXISTS Suppliers (
-    SupplierID INTEGER PRIMARY KEY AUTOINCREMENT,
-    Name TEXT NOT NULL,
-    ContactInfo TEXT
+-- Create EmployeeRecords table
+CREATE TABLE EmployeeRecords (
+  EmployeeID INT PRIMARY KEY AUTO_INCREMENT,
+  Name VARCHAR(100)
 );
 
--- Products
-CREATE TABLE IF NOT EXISTS Products (
-    ProductID INTEGER PRIMARY KEY AUTOINCREMENT,
-    Name TEXT NOT NULL,
-    Description TEXT,
-    Category TEXT,
-    SupplierID INTEGER,
-    FOREIGN KEY (SupplierID) REFERENCES Suppliers (SupplierID) ON DELETE SET NULL ON UPDATE CASCADE
+-- Create Suppliers table
+CREATE TABLE Suppliers (
+  SupplierID INT PRIMARY KEY AUTO_INCREMENT,
+  Name VARCHAR(100),
+  Contact VARCHAR(100),
+  Email VARCHAR(100)
 );
 
--- Inventory
-CREATE TABLE IF NOT EXISTS Inventory (
-    InventoryID INTEGER PRIMARY KEY AUTOINCREMENT,
-    ProductID INTEGER NOT NULL,
-    QuantityAvailable INTEGER NOT NULL CHECK (QuantityAvailable >= 0),
-    FOREIGN KEY (ProductID) REFERENCES Products (ProductID) ON DELETE CASCADE ON UPDATE CASCADE
+-- Create ProductType table
+CREATE TABLE ProductType (
+  TypeID INT PRIMARY KEY AUTO_INCREMENT,
+  CatalogueCode VARCHAR(50),
+  Barcode VARCHAR(50),
+  UKT_ZED VARCHAR(50),
+  Unit VARCHAR(20),
+  IsImported BOOLEAN,
+  SupplierID INT,
+  FOREIGN KEY (SupplierID) REFERENCES Suppliers(SupplierID)
 );
 
--- Orders
-CREATE TABLE IF NOT EXISTS Orders (
-    OrderID INTEGER PRIMARY KEY AUTOINCREMENT,
-    Date DATETIME DEFAULT CURRENT_TIMESTAMP,
-    UserID INTEGER NOT NULL,
-    CustomerID INTEGER NOT NULL,
-    Status TEXT NOT NULL,
-    Type TEXT NOT NULL,
-    FOREIGN KEY (UserID) REFERENCES Users (UserID) ON DELETE RESTRICT ON UPDATE CASCADE,
-    FOREIGN KEY (CustomerID) REFERENCES Contacts (ContactID) ON DELETE RESTRICT ON UPDATE CASCADE
+-- Create Products table
+CREATE TABLE Products (
+  ProductID INT PRIMARY KEY AUTO_INCREMENT,
+  Name VARCHAR(100),
+  Description TEXT,
+  Category VARCHAR(50),
+  ProductTypeID INT,
+  EmployeeRecordID INT,
+  FOREIGN KEY (ProductTypeID) REFERENCES ProductType(TypeID),
+  FOREIGN KEY (EmployeeRecordID) REFERENCES EmployeeRecords(EmployeeID)
 );
 
--- Order Details
-CREATE TABLE IF NOT EXISTS OrderDetails (
-    OrderDetailID INTEGER PRIMARY KEY AUTOINCREMENT,
-    OrderID INTEGER NOT NULL,
-    ProductID INTEGER NOT NULL,
-    Quantity INTEGER NOT NULL CHECK (Quantity > 0),
-    Price REAL NOT NULL CHECK (Price >= 0),
-    FOREIGN KEY (OrderID) REFERENCES Orders (OrderID) ON DELETE CASCADE ON UPDATE CASCADE,
-    FOREIGN KEY (ProductID) REFERENCES Products (ProductID) ON DELETE CASCADE ON UPDATE CASCADE
+-- Create Inventory table
+CREATE TABLE Inventory (
+  InventoryID INT PRIMARY KEY AUTO_INCREMENT,
+  ProductID INT,
+  UserID INT,
+  QuantityAvailable INT,
+  FOREIGN KEY (ProductID) REFERENCES Products(ProductID),
+  FOREIGN KEY (UserID) REFERENCES Users(UserID)
 );
 
--- Order History
-CREATE TABLE IF NOT EXISTS OrderHistory (
-    OrderHistoryID INTEGER PRIMARY KEY AUTOINCREMENT,
-    OrderID INTEGER NOT NULL,
-    Status TEXT NOT NULL,
-    Timestamp DATETIME DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (OrderID) REFERENCES Orders (OrderID) ON DELETE CASCADE ON UPDATE CASCADE
+-- Create Orders table
+CREATE TABLE Orders (
+  OrderID INT PRIMARY KEY AUTO_INCREMENT,
+  Date DATETIME,
+  UserID INT,
+  CustomerID INT,
+  Status VARCHAR(50),
+  Type VARCHAR(50),
+  FOREIGN KEY (UserID) REFERENCES Users(UserID),
+  FOREIGN KEY (CustomerID) REFERENCES Contacts(ContactID)
 );
 
--- Password should be hashed using an application-level library like bcrypt or Argon2 before storage
+-- Create OrderDetails table
+CREATE TABLE OrderDetails (
+  OrderDetailID INT PRIMARY KEY AUTO_INCREMENT,
+  OrderID INT,
+  ProductID INT,
+  Quantity INT,
+  Price FLOAT,
+  FOREIGN KEY (OrderID) REFERENCES Orders(OrderID),
+  FOREIGN KEY (ProductID) REFERENCES Products(ProductID)
+);
 
--- Add Triggers, Stored Procedures, and other Constraints (omitted for now)
-
--- Indices for performance
-CREATE INDEX IF NOT EXISTS idx_order_userid ON Orders(UserID);
-CREATE INDEX IF NOT EXISTS idx_orderdetails_orderid ON OrderDetails(OrderID);
+-- Create PurchaseOrders table
+CREATE TABLE PurchaseOrders (
+  PurchaseOrderID INT PRIMARY KEY AUTO_INCREMENT,
+  Date DATETIME,
+  UserID INT,
+  SupplierID INT,
+  Status VARCHAR(50),
+  FOREIGN KEY (UserID) REFERENCES Users(UserID),
+  FOREIGN KEY (SupplierID) REFERENCES Suppliers(SupplierID)
+);
