0ed77df2637a2dc04404ec8a020eef16edee5a34
MaksGMaks <143256484+MaksGMaks@users.noreply.github.com>
2023-11-05 20:53:23 +0200

    fix1
    replace useless code in cpp, add define in hpp

--- _client/src/SpdlogWrapper.cpp
+++ _client/src/SpdlogWrapper.cpp
@@ -5,10 +5,7 @@ namespace SpdlogWrapper
 
     void init(LogLevel level)
     {
-        auto console = spdlog::stdout_color_mt("console");
-        console->set_level(static_cast<spdlog::level::level_enum>(level));
-        console->set_pattern("[%Y-%m-%d %H:%M:%S.%e] [%^%l%$] %v");
-        console->flush_on(static_cast<spdlog::level::level_enum>(level));
+        spdlog::default_logger()->set_level(static_cast<spdlog::level::level_enum>(level));
     }
 
 }
