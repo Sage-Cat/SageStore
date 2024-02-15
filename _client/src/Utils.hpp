46517421746c90747ba041bd4cf7e585d343eab6
MaksGMaks <143256484+MaksGMaks@users.noreply.github.com>
2024-02-15 23:35:16 +0200

    quick fix

--- _client/src/Utils.hpp
+++ _client/src/Utils.hpp
@@ -1,9 +1,44 @@
 #pragma once
 
 #include <QVBoxLayout>
+#include <QHBoxLayout>
+#include <vector>
 
 namespace Utils
 {
-    QVBoxLayout *createVBoxLayout(QWidget *parent = nullptr, std::vector<QWidget *> widgets);
-    QHBoxLayout *createHBoxLayout(QWidget *parent = nullptr, std::vector<QWidget *> widgets);
+    template <typename LayoutItem>
+    QVBoxLayout *createVBoxLayout(QWidget *parent = nullptr, std::vector<LayoutItem *> items = {})
+    {
+        QVBoxLayout *layout = new QVBoxLayout(parent);
+        for (auto *item : items)
+        {
+            if constexpr (std::is_base_of<QLayout, LayoutItem>::value)
+            {
+                layout->addLayout(static_cast<QLayout *>(item));
+            }
+            else if constexpr (std::is_base_of<QWidget, LayoutItem>::value)
+            {
+                layout->addWidget(static_cast<QWidget *>(item));
+            }
+        }
+        return layout;
+    }
+    template <typename LayoutItem>
+    QHBoxLayout *createHBoxLayout(QWidget *parent = nullptr, std::vector<LayoutItem *> items = {})
+    {
+        QHBoxLayout *layout = new QHBoxLayout(parent);
+        for (auto *item : items)
+        {
+            if constexpr (std::is_base_of<QLayout, LayoutItem>::value)
+            {
+                layout->addLayout(static_cast<QLayout *>(item));
+            }
+            else if constexpr (std::is_base_of<QWidget, LayoutItem>::value)
+            {
+                layout->addWidget(static_cast<QWidget *>(item));
+            }
+        }
+        return layout;
+    }
+
 }
