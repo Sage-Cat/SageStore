cb9f6ed33cf33174bd794a5d5c1dfd1f0b02ed5a
our ame <you@example.com>
2024-03-07 12:06:10 +0200

    BoostForMe

--- conanfile.py
+++ conanfile.py
@@ -1,5 +1,5 @@
 from conan import ConanFile
-
+import os
 
 class SageStorePkg(ConanFile):
     name = "sage_store"
@@ -14,9 +14,27 @@ class SageStorePkg(ConanFile):
         "sqlite3/*:threadsafe": 2
     }
 
-    def requirements(self):
+    def requirements(self): 
         self.requires("spdlog/1.12.0")
         self.requires("nlohmann_json/3.11.2")
-        self.requires("boost/1.82.0")
+        if not IS_BOOST_INSTASLLED_HANDLY():
+            self.requires("boost/1.82.0") #Not needed if installed handly
         self.requires("gtest/1.14.0")
         self.requires("sqlite3/3.45.1")
+
+# Check is Boost installed handly
+def IS_BOOST_INSTASLLED_HANDLY():
+    IS_BOOST_INSTALLED = False
+    
+    # Check BOOST_ROOT
+    if "BOOST_ROOT" in os.environ:
+        IS_BOOST_INSTALLED = True
+
+    # Check BOOST in PATH
+    path_list = os.environ.get('PATH', '').split(';')
+    for path in path_list:
+        if 'boost' in path.lower():
+            IS_BOOST_INSTALLED = True
+            break
+    
+    return IS_BOOST_INSTALLED
