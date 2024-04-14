17fd9e81386eb532a33bbe46f461b25052f12a0b
DanyloPlotskykh <145497398+DanyloPlotskykh@users.noreply.github.com>
2024-04-14 21:42:09 +0300

    pipeline

--- _server/src/Database/Entities/Role.hpp
+++ _server/src/Database/Entities/Role.hpp
@@ -13,8 +13,10 @@ public:
 
     Role(std::string id, std::string name)
         : id(std::move(id)), name(std::move(name)) {}
-    explicit Role(std::string name) : name(std::move(name)) {}
-
+    explicit Role(std::string name) : name(std::move(name))
+    {
+    }
+    
     Dataset &operator>>(Dataset &dataset) override
     {
         dataset[Keys::Role::ID].push_back(id.empty() ? "" : id);
