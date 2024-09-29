063994298cb14a3c4e8ccaabd86c5498acc698a2
DanyloPlotskykh <danylo.plotskykh.ki.2022@lpnu.ua>
2024-09-29 20:18:50 +0300

    restyle modified files

--- _server/src/Database/PurchaseOrdersRepository.hpp
+++ _server/src/Database/PurchaseOrdersRepository.hpp
@@ -4,14 +4,13 @@
 #include <optional>
 #include <vector>
 
-#include "IRepository.hpp"
 #include "IDatabaseManager.hpp"
+#include "IRepository.hpp"
 #include "common/Entities/PurchaseOrder.hpp"
 
-using PurchaseOrder  = Common::Entities::PurchaseOrder;
+using PurchaseOrder = Common::Entities::PurchaseOrder;
 
-class PurchaseOrderRepository : public IRepository<Common::Entities::PurchaseOrder>
-{
+class PurchaseOrderRepository : public IRepository<Common::Entities::PurchaseOrder> {
 public:
     explicit PurchaseOrderRepository(std::shared_ptr<IDatabaseManager> dbManager);
     ~PurchaseOrderRepository() override;
@@ -19,11 +18,13 @@ public:
     void add(const PurchaseOrder &entity) override;
     void update(const PurchaseOrder &entity) override;
     void deleteResource(const std::string &id) override;
-    std::vector<PurchaseOrder> getByField(const std::string &fieldName, const std::string &value) const override;
+    std::vector<PurchaseOrder> getByField(const std::string &fieldName,
+                                          const std::string &value) const override;
     std::vector<PurchaseOrder> getAll() const override;
 
 private:
     PurchaseOrder PurchaseFromCurrentRow(const std::shared_ptr<IQueryResult> &queryResult) const;
+
 private:
     std::shared_ptr<IDatabaseManager> m_dbManager;
 };
--- _server/tests/unit/PurchaseOrdersRepositoryTest.cpp
+++ _server/tests/unit/PurchaseOrdersRepositoryTest.cpp
@@ -1,12 +1,12 @@
-#include <gtest/gtest.h>
 #include <gmock/gmock.h>
+#include <gtest/gtest.h>
 
-#include <vector>
 #include <iostream>
+#include <vector>
 
-#include "mocks/DatabaseManagerMock.hpp"
 #include "Database/PurchaseOrdersRepository.hpp"
 #include "ServerException.hpp"
+#include "mocks/DatabaseManagerMock.hpp"
 
 #include "common/Entities/PurchaseOrder.hpp"
 #include "common/Keys.hpp"
@@ -32,17 +32,19 @@ protected:
 TEST_F(PurchaseOrderRepoTest, getAll)
 {
     PurchaseOrder current;
-    current.id = "2";
-    current.date = "12-01-22";
-    current.userId = "47";
+    current.id         = "2";
+    current.date       = "12-01-22";
+    current.userId     = "47";
     current.supplierId = "11";
-    current.status = "sus";
+    current.status     = "sus";
 
-    EXPECT_CALL(*dataMock, executeQuery("SELECT id, date, userId, supplierId, status FROM PurchaseOrder;", ::testing::_))
+    EXPECT_CALL(*dataMock,
+                executeQuery("SELECT id, date, userId, supplierId, status FROM PurchaseOrder;",
+                             ::testing::_))
         .WillOnce(::testing::Return(qureMock));
     EXPECT_CALL(*qureMock, next())
         .WillOnce(::testing::Return(true))
-        .WillOnce(::testing::Return(false)); 
+        .WillOnce(::testing::Return(false));
     EXPECT_CALL(*qureMock, getString(0)).WillOnce(::testing::Return(current.id));
     EXPECT_CALL(*qureMock, getString(1)).WillOnce(::testing::Return(current.date));
     EXPECT_CALL(*qureMock, getString(2)).WillOnce(::testing::Return(current.userId));
@@ -50,8 +52,7 @@ TEST_F(PurchaseOrderRepoTest, getAll)
     EXPECT_CALL(*qureMock, getString(4)).WillOnce(::testing::Return(current.status));
 
     auto data = purchaseOrderRepository->getAll();
-    for(auto purch : data)
-    {
+    for (auto purch : data) {
         ASSERT_EQ(purch.id, current.id);
         ASSERT_EQ(purch.date, current.date);
         ASSERT_EQ(purch.userId, current.userId);
@@ -63,28 +64,30 @@ TEST_F(PurchaseOrderRepoTest, getAll)
 TEST_F(PurchaseOrderRepoTest, getByField)
 {
     PurchaseOrder current;
-    current.id = "2";
-    current.date = "12-01-22";
-    current.userId = "47";
+    current.id         = "2";
+    current.date       = "12-01-22";
+    current.userId     = "47";
     current.supplierId = "11";
-    current.status = "sus";
+    current.status     = "sus";
 
-    EXPECT_CALL(*dataMock, executeQuery("SELECT id, date, userId, supplierId, status FROM PurchaseOrder WHERE status = ?;", ::testing::_))
+    EXPECT_CALL(
+        *dataMock,
+        executeQuery(
+            "SELECT id, date, userId, supplierId, status FROM PurchaseOrder WHERE status = ?;",
+            ::testing::_))
         .WillOnce(::testing::Return(qureMock));
     EXPECT_CALL(*qureMock, next())
         .WillOnce(::testing::Return(true))
         .WillOnce(::testing::Return(false));
-    
+
     EXPECT_CALL(*qureMock, getString(0)).WillOnce(::testing::Return(current.id));
     EXPECT_CALL(*qureMock, getString(1)).WillOnce(::testing::Return(current.date));
     EXPECT_CALL(*qureMock, getString(2)).WillOnce(::testing::Return(current.userId));
     EXPECT_CALL(*qureMock, getString(3)).WillOnce(::testing::Return(current.supplierId));
     EXPECT_CALL(*qureMock, getString(4)).WillOnce(::testing::Return(current.status));
 
-
     auto data = purchaseOrderRepository->getByField("status", "sus");
-    for(auto purch : data)
-    {
+    for (auto purch : data) {
         ASSERT_EQ(purch.id, current.id);
         ASSERT_EQ(purch.date, current.date);
         ASSERT_EQ(purch.userId, current.userId);
@@ -92,4 +95,3 @@ TEST_F(PurchaseOrderRepoTest, getByField)
         ASSERT_EQ(purch.status, current.status);
     }
 }
-
