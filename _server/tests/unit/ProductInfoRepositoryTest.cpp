7cb6cb5b978ed2bc98cb2005073b3f1067781d9f
dontuo <bench.oleg@gmail.com>
2024-11-19 23:09:38 +0200

    add tests for ProductInfoRepository

---
+++ _server/tests/unit/ProductInfoRepositoryTest.cpp
@@ -0,0 +1,96 @@
+#include <algorithm>
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+
+#include <iostream>
+#include <memory>
+#include <string_view>
+#include <vector>
+
+#include "Database/IDatabaseManager.hpp"
+#include "Database/ProductInfoRepository.hpp"
+#include "ServerException.hpp"
+#include "common/Entities/ProductInfo.hpp"
+
+#include "common/Keys.hpp"
+#include "common/SpdlogConfig.hpp"
+
+#include "gmock/gmock.h"
+#include "mocks/DatabaseManagerMock.hpp"
+
+using ::testing::_;
+using ::testing::Invoke;
+using ::testing::Return;
+
+class ProductInfoRepositoryTest : public ::testing::Test {
+protected:
+    std::unique_ptr<ProductInfoRepository> productInfoRepository;
+    std::shared_ptr<DatabaseManagerMock> databaseManagerMock;
+    std::shared_ptr<QueryResultMock> queryResultMock;
+
+    ProductInfoRepositoryTest()
+    {
+        queryResultMock       = std::make_shared<QueryResultMock>();
+        databaseManagerMock   = std::make_shared<DatabaseManagerMock>();
+        productInfoRepository = std::make_unique<ProductInfoRepository>(databaseManagerMock);
+    }
+};
+
+TEST_F(ProductInfoRepositoryTest, add)
+{
+    const Common::Entities::ProductInfo requestData{
+        .id            = "1",
+        .productTypeId = "10",
+        .name          = "Test_name",
+        .value         = "test value",
+    };
+
+    EXPECT_CALL(*databaseManagerMock, executeQuery(_, _)).WillRepeatedly(Return(queryResultMock));
+
+    productInfoRepository->add(requestData);
+}
+
+TEST_F(ProductInfoRepositoryTest, update)
+{
+    const Common::Entities::ProductInfo requestData{
+        .id            = "1",
+        .productTypeId = "10",
+        .name          = "Test_name",
+        .value         = "test value",
+    };
+
+    EXPECT_CALL(*databaseManagerMock, executeQuery(_, _)).WillRepeatedly(Return(queryResultMock));
+
+    productInfoRepository->update(requestData);
+}
+
+TEST_F(ProductInfoRepositoryTest, deleteResource)
+{
+    const std::string requestData{"1"};
+
+    EXPECT_CALL(*databaseManagerMock, executeQuery(_, _)).WillRepeatedly(Return(queryResultMock));
+
+    productInfoRepository->deleteResource(requestData);
+}
+
+TEST_F(ProductInfoRepositoryTest, getByField)
+{
+    EXPECT_CALL(*databaseManagerMock, executeQuery(_, _)).WillRepeatedly(Return(queryResultMock));
+
+    productInfoRepository->getByField("", "");
+}
+
+TEST_F(ProductInfoRepositoryTest, getAll)
+{
+    EXPECT_CALL(*databaseManagerMock, executeQuery(_, _)).WillRepeatedly(Return(queryResultMock));
+
+    productInfoRepository->getAll();
+}
+
+int main(int argc, char **argv)
+{
+    SpdlogConfig::init<SpdlogConfig::LogLevel::Off>();
+    ::testing::InitGoogleMock(&argc, argv);
+
+    return RUN_ALL_TESTS();
+}
